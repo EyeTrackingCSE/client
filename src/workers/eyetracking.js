45c81fafa483f732ebac8056f547028c54aed6de
vardaro <anthonyavardaro67@gmail.com>
2020-12-22 22:08:05 -0600

    This is cursed

--- src/workers/eyetracking.js
+++ src/workers/eyetracking.js
@@ -23,25 +23,3 @@ export const listen = (cb) => {
 
     screen.listen(cb);
 }
-
-export const calculatePrimes = (iterations, multiplier) => {
-    while(true)  {
-      let primes = [];
-      for (let i = 0; i < iterations; i++) {
-        let candidate = i * (multiplier * Math.random());
-        let isPrime = true;
-        
-        for (var c = 2; c <= Math.sqrt(candidate); ++c) {
-          if (candidate % c === 0) {
-            // not prime
-            isPrime = false;
-            break;
-          }
-        }
-        if (isPrime) {
-          primes.push(candidate);
-        }
-      }
-      postMessage(primes);
-    }
-  }
