bb932576ed80d4deb5b1c93f55b0a6a08d4675f4
vardaro <anthonyavardaro67@gmail.com>
2020-12-24 19:24:00 -0600

    Namespace improvements

--- src/start.js
+++ src/start.js
@@ -3,7 +3,11 @@ const eyetracking = require('eyetracking');
 const path = require('path')
 const url = require('url')
 
-const constants = require('./constants/index');
+const { ASYNC_GAZE_FOCUS_EVENT,
+  ASYNC_LISTEN,
+  SYNC_SET_SCREEN_SPACE,
+  OK,
+  ERROR } = require('./constants/index');
 
 let mainWindow
 
@@ -11,7 +15,6 @@ let screen = null;
 
 // Electron window params, we can config this to better fit the use of our app.
 function createWindow() {
-
   mainWindow = new BrowserWindow({
     width: 1920,
     height: 1080,
@@ -57,35 +60,35 @@ app.on('activate', () => {
  * Actually creates a new eyetracking instance, so old data is disregarded.
  * Pushes keys to node-gyp module.
  */
-ipcMain.on(constants.SYNC_SET_GAZE_FOCUS_REGIONS, (event, arg) => {
+ipcMain.on(SYNC_SET_GAZE_FOCUS_REGIONS, (event, arg) => {
   if (!arg.rectangles.length) {
-    event.returnValue = constants.ERROR;
+    event.returnValue = ERROR;
     return;
   }
 
   screen = new eyetracking(arg.width, arg.height);
   screen.AddRectangles(arg.rectangles);
-  event.returnValue = constants.OK;
+  event.returnValue = OK;
 });
 
 /**
  * Sets only the screen space size of the monitor.
  */
-ipcMain.on(constants.SYNC_SET_SCREEN_SPACE, (event, arg) => {
+ipcMain.on(SYNC_SET_SCREEN_SPACE, (event, arg) => {
   if (screen === null) {
     event.returnValue = constants.ERROR;
     return;
   }
 
   if (!arg.height || !arg.width) {
-    event.returnValue = constants.ERROR;
+    event.returnValue = ERROR;
     return;
   }
 
   screen.SetWidth(arg.width);
   screen.SetHeight(arg.height);
 
-  event.returnValue = constants.OK;
+  event.returnValue = OK;
 });
 
 /**
@@ -95,7 +98,7 @@ ipcMain.on(constants.SYNC_SET_SCREEN_SPACE, (event, arg) => {
  * the metadata of said event to emitted to the renderer process
  * using a event.reply() call. 
  */
-ipcMain.on(constants.ASYNC_LISTEN, (event, arg) => {
+ipcMain.on(ASYNC_LISTEN, (event, arg) => {
 
 });
 
