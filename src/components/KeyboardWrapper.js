f5ee1ceb6740aa0c735e793c04d265989f339236
vardaro <anthonyavardaro67@gmail.com>
2020-12-24 19:36:19 -0600

    Add some useful comments

--- src/components/KeyboardWrapper.js
+++ src/components/KeyboardWrapper.js
@@ -5,8 +5,6 @@ import 'react-simple-keyboard/build/css/index.css';
 
 import "../styles/KeyboardWrapper.css";
 
-import * as constants from "../constants/index";
-
 import {
   ASYNC_GAZE_FOCUS_EVENT,
   SET_GAZE_FOCUS_REGIONS,
@@ -20,16 +18,18 @@ const KeyboardWrapper = () => {
   const [layout, setLayout] = useState("default");
   const keyboard = useRef();
 
+  /**
+   * Gets called when component mounts
+   * Just binds the 'resize' event to setKeyDimensions to update eyetracker
+   */
   useEffect(() => {
     window.addEventListener('resize', setKeyDimensions);
-    ipcRenderer.on(ASYNC_GAZE_FOCUS_EVENT, onGazeFocusEvent);
-
-    // ipcRenderer.sendSync('asynchronous-message', 'ping')
-    // ipcRenderer.on('asynchronous-reply', (event, arg) => {
-    //   console.log(arg) // prints "pong"
-    // })
   }, []);
 
+  /**
+   * Extracts width of screen, height of screen,
+   * and coordinates of key on virutal keyboard.
+   */
   const getKeyDimensions = () => {
     let w = window.outerWidth;
     let h = window.outerHeight;
@@ -54,6 +54,14 @@ const KeyboardWrapper = () => {
     };
   }
 
+  /**
+   * Gets screen and keyboard metadata, and pushes said info to
+   * the eyetracking module via ipc.
+   * 
+   * Begins eyetracking listen loop.
+   * 
+   * On each gaze focus event, calls onGazeFocusEvent()
+   */
   const setKeyDimensions = () => {
     let dims = getKeyDimensions();
     let returnVal = ipcRenderer.sendSync(SET_GAZE_FOCUS_REGIONS, dims);
@@ -65,22 +73,39 @@ const KeyboardWrapper = () => {
     // Start Tobii listen loop
     ipcRenderer.send(ASYNC_LISTEN, 1);
 
+    ipcRenderer.on(ASYNC_GAZE_FOCUS_EVENT, onGazeFocusEvent);
   }
 
+  /**
+   * TODO
+   * @param {object} event event obj
+   * @param {object} arg args to the ipc event
+   */
   const onGazeFocusEvent = (event, arg) => {
 
   }
 
+  /**
+   * Update input variable in state
+   * @param {string} input 
+   */
   const onChange = input => {
     setInput(input);
     // console.log("Input changed ", input);
   };
 
+  /**
+   * Swaps keyboard to shift mode.
+   */
   const handleShift = () => {
     const newLayout = layout === "default" ? "shift" : "default";
     setLayout(newLayout);
   };
 
+  /**
+   * Called when keyboard button is pressed. Check for shift or caps lock
+   * @param {string} button button pressed
+   */
   const onKeyPress = button => {
     // console.log("Button pressed", button);
 
@@ -89,6 +114,11 @@ const KeyboardWrapper = () => {
     }
   }
 
+  /**
+   * Called when input must change due to manual typing
+   * in <textarea /> window
+   * @param {object} event 
+   */
   const onChangeInput = event => {
     const input = event.target.value;
     setInput(input);
