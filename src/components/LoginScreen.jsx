b2671ebeff0bafd8495110a6109597c581323140
vardaro <anthonyavardaro67@gmail.com>
2021-01-08 16:38:23 -0600

    Add login screen component

---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+const KeyboardWrapper = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default KeyboardWrapper;
