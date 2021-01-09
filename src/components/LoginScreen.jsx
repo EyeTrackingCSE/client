246b6f385c6a941220d50876d96cd1d8e9335cbe
vardaro <anthonyavardaro67@gmail.com>
2021-01-09 16:07:58 -0600

    Fix export in login component

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-const KeyboardWrapper = () => {
+const LoginScreen = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -11,4 +11,4 @@ const KeyboardWrapper = () => {
     );
 }
 
-export default KeyboardWrapper;
+export default LoginScreen;
