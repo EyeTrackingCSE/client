6a65372530fba29a1fa907a228553687a9fe9d1d
vardaro <anthonyavardaro67@gmail.com>
2021-01-23 11:46:20 -0600

    Update API for Clip component

--- src/components/Clip.jsx
+++ src/components/Clip.jsx
@@ -1,7 +1,24 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-const Clip = () => {
+const Clip = (props) => {
+    if (!props.string)
+        throw new Error("string cannot be null/undefined");
 
+    const onClipButtonClick = () => {
+        if (props.onBeforeClip)
+            props.onBeforeClip(props.string);
+
+        navigator.clipboard.writeText(this.state.working);
+        
+        if (props.onAfterClip)
+            props.onAfterClip(props.string);
+    }
+
+    return (
+        <button onClick={onClipButtonClick}>
+            Clip
+        </button>
+    );
 };
 
 export const Clip;
