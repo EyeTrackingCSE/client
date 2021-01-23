9ed1330f78ad20881ce8752af8436d9008d5ef99
vardaro <anthonyavardaro67@gmail.com>
2021-01-22 21:56:33 -0600

    Update proximity-filter script. still work in progress

--- scripts/proximity-filter.js
+++ scripts/proximity-filter.js
@@ -1,3 +1,4 @@
+/* this file is mostly for my sanity, ignore it. -anthony */
 let model = require('../src/constants/index').lettering;
 
 let proximityArray = [
@@ -9,8 +10,15 @@ let proximityArray = [
 
     ['d', ['x', 's', 'e', 'r', 'f', 'c']],
 
-    ['a', ['q', 'w', 's', 'x', 'z']],
+    ['e', ['d', 's', 'w', 'r', 'f']],
 
-    ['a', ['q', 'w', 's', 'x', 'z']],
+    ['f', ['c', 'd', 'r', 't', 'g', 'v']],
+];
 
-]
+let qwerty = [
+    '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+    '{tab} q w e r t y u i o p [ ] \\',
+    '{lock} a s d f g h j k l ; \' {enter}',
+    '{shift} z x c v b n m , . / {shift}',
+    '.com @ {space}'
+];
