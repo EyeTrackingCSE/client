39ea56133af79779521210fdfb2e259a5f40fdb6
vardaro <anthonyavardaro67@gmail.com>
2021-01-22 21:37:06 -0600

    Add scripts

---
+++ scripts/proximity-filter.js
@@ -0,0 +1,16 @@
+let model = require('../src/constants/index').lettering;
+
+let proximityArray = [
+    ['a', ['q', 'w', 's', 'x', 'z']],
+
+    ['b', ['v', 'g', 'h', 'b']],
+
+    ['c', ['x', 'd', 'f', 'v']],
+
+    ['d', ['x', 's', 'e', 'r', 'f', 'c']],
+
+    ['a', ['q', 'w', 's', 'x', 'z']],
+
+    ['a', ['q', 'w', 's', 'x', 'z']],
+
+]
