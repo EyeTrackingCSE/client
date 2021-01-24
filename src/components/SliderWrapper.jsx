b1e9f93db6e4d5335b055e9d6ce57f91d65c4da1
vardaro <anthonyavardaro67@gmail.com>
2021-01-24 15:14:37 -0600

    Add handle function component to handle slider tooltip.

--- src/components/SliderWrapper.jsx
+++ src/components/SliderWrapper.jsx
@@ -1,13 +1,31 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-import Slider, { Range } from 'rc-slider';
+import Slider, { SliderTooltip } from 'rc-slider';
+
 import 'rc-slider/assets/index.css';
 import { defaults } from "../constants/index";
 
+import "../styles/SliderWrapper.css";
+
+const { Handle } = Slider;
+
+
+const handle = props => {
+    const { value, dragging, index, ...restProps } = props;
+    return (
+        <SliderTooltip
+            prefixCls="rc-slider-tooltip"
+            overlay={`${value} %`}
+            visible={dragging}
+            placement="top"
+            key={index}
+        >
+            <Handle value={value} {...restProps} />
+        </SliderTooltip>
+    );
+};
+
 const SliderWrapper = (props) => {
-
-
-
     const formatDwellTimeIntervals = () => {
         let marksArray = defaults.DEFAULT_DWELL_TIME_OPTIONS_MS;
         let marks = {};
@@ -16,13 +34,20 @@ const SliderWrapper = (props) => {
         return marks;
     }
 
+    const onMarkChange = valueMS => {
+
+    };
+
     return (
-        <div>
-            <Slider 
-            min={defaults.DEFAULT_DWELL_TIME_OPTIONS_MS[0]} 
-            defaultValue={defaults.DEFAULT_DWELL_TIME_MS} 
-            marks={formatDwellTimeIntervals()} step={null} />
+        <div className={"wrapper"}>
+            <Slider
+                min={defaults.DEFAULT_DWELL_TIME_OPTIONS_MS[0]}
+                defaultValue={defaults.DEFAULT_DWELL_TIME_MS}
+                marks={formatDwellTimeIntervals()}
+                step={null}
+                handle={handle} />
         </div>
+
     );
 };
 
