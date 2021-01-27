e9cb11f200641aac0d356b91b63d14c692c99ab9
vardaro <anthonyavardaro67@gmail.com>
2021-01-27 12:01:17 -0600

    Add block component

--- src/components/WordSuggestions.jsx
+++ src/components/WordSuggestions.jsx
@@ -1,36 +1,60 @@
 import React, { useState, useEffect } from 'react';
+import "../styles/WordSuggestions.css";
 
 const TIMEOUT_FETCH = 1000; // After this much time, fail fetching and return empty
 const EMPTY_OBJ = {};
+const API = "https://api.datamuse.com/sug?s="
 
 const SEPARATORS = /[\W_]+/
 
-// Word suggestion API from https://www.datamuse.com/api/
-const api = "https://api.datamuse.com/sug?s="
+const block = (props) => {
+    return (
+        <div
+            className={"block"}>
+            {props.word || ''}
+        </div>
+    )
+}
 
 const WordSuggestions = (props) => {
     let [suggestions, setSuggestions] = useState([]);
 
+    /**
+     * Extract the last word in a string.
+     * gamblers ruin => ruin
+     * @param {string} input 
+     */
     const getLastWordFromInput = input => {
         const words = input.split(SEPARATORS);
         const last = (words.length === 0) ? "" : words.pop();
         return last;
     };
 
+    /**
+     * Returns and array of word suggestions
+     * @param {string} input string to query words suggestions for.
+     */
     const getWordSuggestions = async (input) => {
         const controller = new AbortController();
         const signal = controller.signal;
 
-        setTimeout(() => controller.abort(), TIMEOUT_FETCH, EMPTY_OBJ);
+        let timeout = setTimeout(() => controller.abort(), TIMEOUT_FETCH, EMPTY_OBJ);
+        let context = getLastWordFromInput(input);
 
-        let cue = getLastWordFromInput(input);
-
-        let resp = await fetch(api + cue, { signal });
-        let array = await resp.json();
-
-        return array || [];
+        let array = [];
+        try {
+            let resp = await fetch(API + context, { signal });
+            array = await resp.json();
+            clearTimeout(timeout);
+        } catch (e) {
+            console.error(e);
+        }
+        return array;
     };
 
+    /**
+     * Function runs when props.input changes, because we need to query the API again.
+     */
     useEffect(async () => {
         let words = await getWordSuggestions(props.input);
         console.log(words);
@@ -38,7 +62,11 @@ const WordSuggestions = (props) => {
     }, [props.input]);
 
     return (
-        <div></div>
+        <div>
+            <block word={suggestions[0]} />
+            <block word={suggestions[1]} />
+            <block word={suggestions[2]} />
+        </div>
     );
 };
 
