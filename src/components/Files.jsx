3538857c7e96039b0758ff3048f5e6af19e3f849
vardaro <anthonyavardaro67@gmail.com>
2021-02-01 11:58:49 -0600

    Add handler for Files opes

--- src/components/Files.jsx
+++ src/components/Files.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import "../styles/Files.css";
+
+const { dialog } = require('electron').remote
 
 const Files = props => {
 
+    const onSaveClick = e => {
+
+    };
+
+    const onLoadClick = e => {
+
+    };
+
+    return (
+        <div>
+            <div onClick={onSaveClick} className={"file-button"}>
+                Save
+            </div>
+
+            <div onClick={onLoadClick} className={"file-button"}>
+                Load
+            </div>
+        </div>
+    );
 };
 
 export default Files;
