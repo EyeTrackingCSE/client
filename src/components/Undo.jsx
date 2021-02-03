1bdd655fceb7f37420202d241f612484a969cffa
vardaro <anthonyavardaro67@gmail.com>
2021-02-03 10:56:51 -0600

    Add Undo component

---
+++ src/components/Undo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import "../styles/Undo.css";
+
+/**
+ * @param {object} props 
+ */
+const Undo = (props) => {
+    return (
+        <div onClick={} className={""}>
+            Undo
+        </div>
+    );
+};
+
+export default Undo;
