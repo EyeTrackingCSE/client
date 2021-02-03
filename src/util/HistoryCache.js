5197a226d4071a7b71458dbc9b6156a3b46cccdc
vardaro <anthonyavardaro67@gmail.com>
2021-02-03 10:40:01 -0600

    Implement undo in History cache

--- src/util/HistoryCache.js
+++ src/util/HistoryCache.js
@@ -1,10 +1,10 @@
-const INSERT = 'U';
-const DELETE = 'D';
+const INSERT = 0;
+const DELETE = 1;
 
 class Update {
-    constructor(command, change) {
+    constructor(command, char) {
         this.command = command;
-        this.change = string;
+        this.char = char;
     }
 }
 
@@ -15,6 +15,33 @@ class HistoryCache {
     }
 
     update(newString) {
+        let diff = newString.replace(this.string, '');
 
+        let command;
+        let char;
+        if (diff === newString) {
+            command = DELETE;
+            char = this.string.replace(newString, '');
+        } else {
+            command = INSERT;
+            char = diff;
+        }
+
+        this.history.push(new Update(command, char));
+        this.string = newString;
+    }
+
+    undo() {
+        if (this.history.length === 0)
+            return '';
+
+        let last = this.history.pop();
+        let newString = '';
+
+        if (last.command === INSERT) {
+            newString = this.string.slice(0, -1);
+        } else {
+            newString = this.string + last.change;
+        }
     }
 }
