20b4e9ba75cc94d915d588d6e14eec5b73ef826e
vardaro <anthonyavardaro67@gmail.com>
2021-03-14 17:18:23 -0300

    Add def layouts

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -38,5 +38,22 @@ module.exports = {
 
     DEFAULT_TOBII_EXPERIENCE_PATH: "c:\\Program Files (x86)\\Tobii",
 
-    DEFAULT_RECALIBRATE_TIMER: 30000
+    DEFAULT_RECALIBRATE_TIMER: 30000,
+
+    DEFAULT_LAYOUTS: {
+        'default': [
+            '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+            '{tab} q w e r t y u i o p [ ] \\',
+            '{lock} a s d f g h j k l ; \' {enter}',
+            '{shift} z x c v b n m , . / {shift}',
+            '.com @ {space}'
+        ],
+        'shift': [
+            '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+            '{tab} Q W E R T Y U I O P { } |',
+            '{lock} A S D F G H J K L : " {enter}',
+            '{shift} Z X C V B N M < > ? {shift}',
+            '.com @ {space}'
+        ]
+    }
 }
